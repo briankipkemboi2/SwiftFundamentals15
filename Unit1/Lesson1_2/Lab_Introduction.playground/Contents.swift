1e36d7ba134da2148f4707525f7473dafd745c15
briankipkemboi2 <briankipkemboi800@gmail.com>
2025-10-26 15:57:07 -0400

    Print song title and lyrics to console
    Added print statements for song title and lyrics.

--- Unit1/Lesson1_2/Lab_Introduction.playground/Contents.swift
+++ Unit1/Lesson1_2/Lab_Introduction.playground/Contents.swift
@@ -12,11 +12,14 @@ print("How to use playgrounds to make writing Swift fun and simple")
 /*:
  Now print your own phrases to the console. Pick one of your favorite songs. Use your knowledge of the `print` function to display the song title and artist.
  */
-
+print("Commas by Ayra Starr")
 
 /*:
  Use multiple `print` functions to write out some of the lyrics to the song.
  */
+print("I carry God, So I fear nothing")
+print("Steady increasing the Commas")
+ print("Comma, Comma, Commas")
 
 
 //:page 14 of 16  |  [Next: Exercise: Go! Fight! Win!](@next)
