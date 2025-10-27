5ffab90c0c449a0800b88dfe79520663d315e5b8
briankipkemboi2 <briankipkemboi800@gmail.com>
2025-10-26 19:08:11 -0500

    Refactor constants and variables in Contents.swift
    Updated constants and variables in Swift playground with proper assignments and print statements.

--- Unit1/Lesson1_3/Lab_Constants_Variables.playground/Contents.swift
+++ Unit1/Lesson1_3/Lab_Constants_Variables.playground/Contents.swift
@@ -3,27 +3,30 @@
  
  Declare a constant called `friends` to represent the number of friends you have on social media. Give it a value between 50 and 1000. Print out the value by referencing your constant.
  */
-
+let friends = 999
+print(friends)
 
 /*:
  Now assume you go through and remove a lot of your friends that aren't active on social media. Update your `friends` constant to a lower number than it currently is between 1 and 900.
  */
-
+// friends = 450
 
 /*:
  Does the above code compile? Why not? Print your explanation to the console using the `print` function. Go back and delete your line of code that updates the `friend` constant to a lower number so that the playground will compile properly.
  */
 
-
+print("The code does not compile because friends are declared as friends with let and constants cannot be changed after they are declared
 /*:
  Declare a variable `age` and set it to your own age. Print `age` to the console.
  */
-
+var age = 28
+print(age)
 
 /*:
  Now pretend you just had a birthday, and update the `age` variable accordingly. Print `age` to the console.
  */
-
+age = 29
+      print("After my birthday, my age is now \(age) ")
 /*:
  
  */
@@ -32,19 +35,36 @@
 /*:
  Create a double variable with a value of 1.1. Update it to 2.2, 3.3, and 4.4, printing out the value after each assignment (again by referencing the variable you created).
  */
-
+      // but double is a primitive type, i think thats why its not compiling
+var double = 1.1
+ print(double)
+ double = 2.2
+ print(double)
+ double= 3.3
+ print(double)
+ double = 4.4
+ print(double)
+      
 
 /*:
  Create a boolean variable and set it to `true`. Print the variable, then assign it a value of `false`, and print it again.
  */
-
+var drink = true
+print(drink)
+ drink = false 
+ print(drink)
 
 /*:
  Create two variables, one with a value of 0, the other with a value of 0.0. Try to assign the second variable to the first, and you will receive an error. Add the necessary type annotation that will allow the second variable to be assigned to the first.
  */
+var intValue : Int = 0
+var doubleValue = 0.0
 
+intValue = Int(doubleValue)
+print(intValue)
 
 /*:
  Create a variable integer with a value of 1,000,000,000, ensuring that you format it so it is more readable (i.e. it's hard to read 1000000000, so make it easier to read).
  */
-
+var val = 1,000,000,000
+print(val)
